f2df34bd6715c63acf2009b192490fbdb6723039
Ravil Tuychin <tuychin@gmail.com>
2020-11-18 03:19:54 +0300

    fix(full-page-scroll): text transition on page scroll

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -45,7 +45,9 @@ export default class FullPageScroll {
       screen.classList.remove(`active`);
     });
     this.screenElements[this.activeScreen].classList.remove(`screen--hidden`);
-    this.screenElements[this.activeScreen].classList.add(`active`);
+    setTimeout(() => {
+      this.screenElements[this.activeScreen].classList.add(`active`);
+    }, 100);
   }
 
   changeActiveMenuItem() {
